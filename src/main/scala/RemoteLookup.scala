407ea5c13cf7f3ca62f80cd1e3406bd7c05e0289
Timothy Klim <klimtimothy@gmail.com>
2012-07-11 21:31:25 +0400

    update

--- src/main/scala/RemoteLookup.scala
+++ src/main/scala/RemoteLookup.scala
@@ -3,6 +3,6 @@ import java.io.Serializable
 
 object RemoteLookup extends App {
   val system = ActorSystem("pullbox")
-  val greeter = system.actorFor("pullbox@localhost:2552/user/greeter")
+  val greeter = system.actorFor("pullbox@pullbox.com:2552/user/greeter")
   greeter ! Greeting("Sonny Rollins")
 }
